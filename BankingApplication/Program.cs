bba93f1b05812966d063bd11d923adaa0ed1af2a
Traci Chan <tlc668@gmail.com>
2020-02-12 16:32:44 -0500

    Add project files.

---
+++ BankingApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using BankingLibrary;
+using System;
+
+namespace BankingApplication {
+    class Program {
+        static void Main(string[] args) {
+
+            //test data
+            var chk = new Account();
+            chk.Deposit(100);
+            chk.Withdrawl(75);
+
+        }
+    }
+}
---
+++ BankingLibrary/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankingLibrary {
+    public class Account {
+
+       
+        public string AccountNumber { get; set; }
+        public string Desc { get; set; }
+        public decimal Balance { get; set; }
+        
+        public void Deposit(decimal amount) {
+            Balance += amount;
+
+        }
+        public void Withdrawl(decimal amount) {
+            Balance -= amount;
+
+        }
+        public void Transfer(decimal amount, Account toAcct, Account fromAcct) {
+            //withdrawl from acct
+            fromAcct.Withdrawl(amount);
+            toAcct.Deposit(amount);
+
+        }
+
+        public Account() { }//constructor
+
+    }
+}
